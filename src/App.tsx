807a4bae2522677dc09f19cebb0cce45500b8fea
Guillermo <guillermo.fraile@outlook.com>
2026-04-24 20:48:24 +0200

    add: Terms page with comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/DashboardPage";
 import PrivacyPage from "./pages/PrivacyPage";
+import TermsPage from "./pages/TermsPage";
 
 /**
  * Define el sistema de enrutamiento de la aplicación Fitwell.
@@ -16,6 +17,7 @@ export default function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/terms" element={<TermsPage />} />
       <Route path="/privacy" element={<PrivacyPage />} />
 
       {/* RUTA PROTEGIDAS: si no hay sesión, ProtectedRoute redirigirá al Login.*/}
